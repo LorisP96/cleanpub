5b6f0016d23df66982c3d8711e4ee16d225c81a9
LorisP96 <lorispalazzo96@gmail.com>
2023-01-09 11:58:58 +0100

    custom object and hook

---
+++ cartridges/custom_cleanpub/cartridge/scripts/hooks/contactAuthor.js
@@ -0,0 +1,30 @@
+'use strict';
+//Fare il require di mail e lo assegna a Mail
+var Mail = require('dw/net/Mail');
+
+/**
+ * Send contact form data to client
+ * @param {Array} contactDetails  - The contact form data
+ * @return void
+ */
+function contactAuthor(contactDetails) {
+
+    //Creare una stringa content prendendo gli elementi di contactDetails
+    var content = contactDetails[message];
+
+    //creiamo una nuova mail e settiamo gli elementi addTo setFrom setSubject e setContent, infine inviamo la mail
+    var mail = new Mail();
+    mail.addTo(contactDetails[email]);
+    mail.setFrom("noreply@cleanpub.online");
+    mail.setSubject("CLEANPUB SITE CONTACT FORM");
+
+    mail.setContent(content);
+    mail.send();
+
+    return;
+}
+
+//facciamo l'export di contactAuthor
+module.exports = {
+    contactAuthor
+}
